66dcded141e3498901c14ff09be06a53996e6377
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-04 22:36:05 +0000

    adding_file

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Set up the ssh configuration file
 exec { 'configuration_file':
-  command => 'sed -i "s/PasswordAuthentication yes/PasswordAuthentication no", /etc/ssh/ssh_config',
-  path    => ['/bin'],
+  command => 'sed -i "s|PasswordAuthentication yes|PasswordAuthentication no", /etc/ssh/ssh_config',
+  path    => '/bin',
 }
