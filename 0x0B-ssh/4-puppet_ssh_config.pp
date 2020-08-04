3c7af6f0ed766bd79cb1dc1d8260b82a6a2b3c94
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-08-05 00:21:25 +0200

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Set up the ssh configuration file
 exec { 'configuration_file':
   command => 'echo "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no\n" >> /etc/ssh/ssh_config',
-  path    => ['/bin',],
+  path    => ['/bin'],
 }
