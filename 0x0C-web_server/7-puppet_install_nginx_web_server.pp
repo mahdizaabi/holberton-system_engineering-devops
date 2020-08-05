2441636d6c031d10cad143a96a3b66f81f548b4f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-05 23:59:28 +0000

    adding_file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+#puppet manifest to set up nginx webserver on the remote machine
+
+package { 'nginx':
+ensure  => latest,
+}
+
+exec { 'execution':
+command => 'nginx start',
+path    => '/etc/init.d'
+}
+
+file_line { 'sudo_rule':
+path => '/etc/nginx/sites-available/default',
+line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+exec { 'restart':
+command => 'nginx restart',
+path    => '/etc/init.d',
+}
