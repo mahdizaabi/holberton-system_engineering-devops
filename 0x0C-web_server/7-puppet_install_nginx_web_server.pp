ab959ed829ccafa3944139e1acfc663557fa91c5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-06 00:50:57 +0000

    adding_file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,11 +5,12 @@ ensure  => installed,
 }
 
 file { '/var/www/html/index.nginx-debian.html':
+ensure  => present,
 content => 'Holberton School',
 }
 
 file_line { 'redirection':
-ensure => 'present',
+ensure => present,
 path   => '/etc/nginx/sites-available/default',
 after  => 'listen 80 default_server;',
 line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
