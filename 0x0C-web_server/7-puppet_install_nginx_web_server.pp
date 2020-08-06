6eccdd0ef560a13dbd3875ac4159473f54dbd341
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-06 00:34:20 +0000

    adding_file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,9 +14,10 @@ content => 'Holberton School',
 }
 
 file_line { 'redirection':
-path  => '/etc/nginx/sites-available/default',
-after => 'listen 80 default_server;',
-line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+ensure => present,
+path   => '/etc/nginx/sites-available/default',
+after  => 'listen 80 default_server;',
+line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
