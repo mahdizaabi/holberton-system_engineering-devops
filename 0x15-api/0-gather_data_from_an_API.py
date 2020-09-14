e55d2f144f2fcb08d8a25f390551e2988e935138
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-14 17:42:11 +0100

    add

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     user_id = argv[1]
     listx = []
     get_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                            format(user_id), verify=False).json()
-    tasks = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(user_id), verify=False).json()
-    for task in tasks:
+                            format(user_id)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(user_id)).json()
+    for task in todo:
         if task.get('completed'):
             listx.append(task.get('title'))
     print("Employee {} is done with tasks({}/{}):".
-          format(get_user.get('name'), len(listx), len(tasks)))
+          format(get_user.get('name'), len(listx), len(todo)))
     for i in listx:
         print("\t{}".format(i))
