4620654af0a674aa244edd5f6c9ff5d72540117c
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-14 20:14:27 +0100

    add_file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Retrieve the user and the corresponding done tasks
+"""
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    userId = argv[1]
+    task_titel = []
+    data = ''
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId)).json()
+    tsks = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId)).json()
+
+    lcsv = []
+    for i in tsks:
+        lcsv.append([userId, user.get('name'), i['completed'], i['title']])
+    myFile = open('{}.csv'.format(userId), 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(lcsv)
