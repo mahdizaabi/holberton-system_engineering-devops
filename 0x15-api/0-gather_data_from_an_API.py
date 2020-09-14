7bb4568f942134f63ce12ffcdafa0e2ab340b891
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-14 18:22:24 +0100

    add

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Retrieve the user and the corresponding done tasks
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId)).json()
+    task_titel = []
+    for task in todo:
+        if task.get('completed') is True:
+            task_titel.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_titel), len(todo)))
+    print("\n".join("\t {}".format(task) for task in task_titel))
