41a7d7bf6d35fdbe317d4d90acac87839ace50d7
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-17 18:21:22 +0000

    add

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains the top_ten function
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after_party=None):
+    header = {'User-Agent': 'fake'}
+    param = {'after': after_party}
+    r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                     headers=header,
+                     params=param)
+    if r.status_code == requests.codes.ok:
+        x = r.json().get('data').get('children')
+        for page in x:
+            hot_list.append(page.get('data').get('title'))
+        after_paty = r.json().get('data').get('after')
+        if after_party is None:
+            return hot_list
+        recurse(subreddit, hot_list, after_party)
+    else:
+        return None
