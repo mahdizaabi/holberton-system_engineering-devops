400c884dda74e05efec015afabb68e7659958dea
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-09-30 16:19:36 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Fix the log Errors, catch the error and fixe it
 exec {'/etc/php5/apache2/php.ini':                                                          
 path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],                                     
 command  => "sed -i '/display_errors = Os/c\display_errors = On' /etc/php5/apache2/php.ini",
