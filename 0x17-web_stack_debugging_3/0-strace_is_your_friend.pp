1d9801336e2958802d343435fcdad5cfe29bdaf0
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-30 14:32:49 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
 # Fix the log Errors, catch the error and fixe it
 exec {'/etc/php5/apache2/php.ini':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => "sed -i '/display_errors = Os/c\display_errors = On' /etc/php5/apache2/php.ini",
-  provider => 'shell',}
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -i '/display_errors = Os/c\display_errors = On' /etc/php5/apache2/php.ini",
+}
 
 exec {'/etc/init.d/apache2':
-  path     => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-  command  => '/etc/init.d/apache2 restart',
-  provider => 'shell',}
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => '/etc/init.d/apache2 restart',
+}
