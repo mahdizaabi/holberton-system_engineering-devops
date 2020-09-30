4c6eff2946acc749e628a089e2d79489f62f8fc5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-30 13:02:08 +0000

    add

---
+++ 0-strace_is_your_friend.pp
@@ -0,0 +1,4 @@
+exec {'/etc/php5/apache2/php.ini':
+path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+command  => 'sed "sed -i '/display_er rors = On/c\display_errors = On'" php.ini',
+provider => 'shell',}
