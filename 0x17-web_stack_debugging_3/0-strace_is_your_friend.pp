d9ae96c05d8e5f9e8bbdc75889a3241c82b418cf
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-30 15:00:44 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,12 @@
-# Fix the log Errors, catch the error and fixe it
+#Fix the log Errors, catch the error and fixe it
 exec {'/etc/php5/apache2/php.ini':
   path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
   command => "sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini",
+}
+
+exec { '/var/www/html/wp-setting.php':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
 
 exec {'/etc/init.d/apache2':
