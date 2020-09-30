0824ad9d96db99f8cf48c7abf5763b484c010d3b
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-30 14:26:51 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,14 @@
 # Fix the log Errors, catch the error and fixe it
 exec {'/etc/php5/apache2/php.ini':
-path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-command  => "sed -i '/display_errors = Os/c\display_errors = On' /etc/php5/apache2/php.ini",
-provider => 'shell',}
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => "sed -i '/display_errors = Os/c\display_errors = On' /etc/php5/apache2/php.ini",
+  provider => 'shell',}
 
 exec { '/var/www/html/wp-setting.php':
   path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
   command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
-exec {'/etc/init.d/apache2':
-path     => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-command  => '/etc/init.d/apache2 restart',
-provider => 'shell',}
+  exec {'/etc/init.d/apache2':
+  path     => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command  => '/etc/init.d/apache2 restart',
+  provider => 'shell',}
