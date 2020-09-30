f0d7f7c6751a2d063ffd0594d9b6ad53fedd7ef1
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-09-30 15:06:36 +0200

    Update 0-strace_is_your_friend.pp

--- 0-strace_is_your_friend.pp
+++ 0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec {'/etc/php5/apache2/php.ini':
 path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-command  => "sed -i '/display_er rors = On/c\display_errors = On' php.ini",
+command  => "sed -i '/display_errors = Off/c\display_errors = On' php.ini",
 provider => 'shell',}
