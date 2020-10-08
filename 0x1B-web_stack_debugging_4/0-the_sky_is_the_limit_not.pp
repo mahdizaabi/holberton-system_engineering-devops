a84ea2137232c9f902ccba2b827277489eb0b089
crxs14 <59511107+mahdizaabi@users.noreply.github.com>
2020-10-08 22:52:30 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,8 @@
+#fix nginx webserver
+
 exec { 'set limit':
-  command => 'sed -i \'s/^ULI.*=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'sed -i \'s/^ULIMIT=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+  path    => '/bin/',
 }
 
 -> exec { 'restart':
